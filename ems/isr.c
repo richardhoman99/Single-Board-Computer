3acd6b8ecff90520a8fd235b6f01b6786eda1edd
richardhoman99 <rhoman99@icloud.com>
2024-02-11 16:57:24 -0600

    Begin monitor development

---
+++ ems/isr.c
@@ -0,0 +1,13 @@
+/*
+ * isr.h
+ * Richard Homan
+ * 02/11/2024
+ * Interrupt service routine definition macros
+ */
+
+#include "isr.h"
+
+extern const int __interrupt_vector[];
+extern void __reset(void);
+
+
---
+++ ems/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ * Richard Homan
+ * 02/11/2024
+ * Entry point for Enemigo Monitor System
+ */
+
+int main(void)
+{
+	int i, j;
+	i = 5;
+	j = i + 3;
+	return 0;
+}
