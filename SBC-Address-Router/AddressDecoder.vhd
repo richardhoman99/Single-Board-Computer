fa1b3e34bd3d41083a04adedeb647336504ccc2a
richardhoman99 <58958273+richardhoman99@users.noreply.github.com>
2024-03-18 11:35:17 -0500

    Add muliplexer to AddressDecoder

--- SBC-Address-Router/AddressDecoder.vhd
+++ SBC-Address-Router/AddressDecoder.vhd
@@ -44,30 +44,61 @@ port(
 end AddressDecoder;
 
 architecture behavioral of AddressDecoder is
--- 0: lds
--- 1: uds
--- 2: addr15
+-- 4: rw
 -- 3: addr16
-signal s_addr_sw : std_logic_vector(3 downto 0);
--- 0 roml
--- 1 romh
--- 2
--- 3
--- 4
-signal s_cs_sw   : std_logic_vector(4 downto 0);
+-- 2: addr15
+-- 1: uds
+-- 0: lds
+signal s_in_sw   : std_logic_vector(4 downto 0);
+-- 5: s_berr
+-- 4: cs_romh
+-- 3: cs_roml
+-- 2: cs_ramh
+-- 1: cs_raml
+-- 0: cs_duart
+signal s_cs_sw   : std_logic_vector(5 downto 0);
+signal s_berr    : std_logic;
 begin
 
---with   select
-	--s_cs_sw <= ""
+-- multiplexer
+-- logic table
+-- a16 a15 uds lds | ber roh rol rah ral drt
+--  0   0   0   0  |  0   x   x   x   x   x 
+--  0   0   0   1  |  1   0   1   0   0   0 
+--  0   0   1   0  |  1   1   0   0   0   0 
+--  0   0   1   1  |  0   x   x   x   x   x 
+--  0   1   0   0  |  0   x   x   x   x   x 
+--  0   1   0   1  |  1   0   0   0   1   0 
+--  0   1   1   0  |  1   0   0   1   0   0 
+--  0   1   1   1  |  0   x   x   x   x   x 
+--  1   0   0   0  |  0   x   x   x   x   x 
+--  1   0   0   1  |  1   0   0   0   0   1 
+--  1   0   1   0  |  0   x   x   x   x   x 
+--  1   0   1   1  |  0   x   x   x   x   x 
+--  1   1   0   0  |  0   x   x   x   x   x 
+--  1   1   0   1  |  0   x   x   x   x   x 
+--  1   1   1   0  |  0   x   x   x   x   x 
+--  1   1   1   1  |  0   x   x   x   x   x 
+with s_in_sw select s_cs_sw <=
+	"101000" when "0001", -- rom low
+	"110000" when "0010", -- rom high
+	"100010" when "0101", -- ram low
+	"100100" when "0110", -- ram high
+	"100001" when "1001", -- duart
+	"0XXXXX" when others;
 
--- enable output only when address is valid
-o_oe <= i_as;
-
-process(i_as)
+-- latch chip selects such that 
+process(i_as, s_berr)
 begin
-	if i_as = '0' then
+	-- as asserted and not a bus error
+	if (i_as and not s_berr) = '0' then
 		-- only set values if i_as is asserted
-		
+		-- bind multiplexer outputs
+		s_cs_sw(4) <= o_cs_romh;
+		s_cs_sw(3) <= o_cs_roml;
+		s_cs_sw(2) <= o_cs_ramh;
+		s_cs_sw(1) <= o_cs_raml;
+		s_cs_sw(0) <= o_cs_duart;
 	else
 		-- set to default values when i_as is not asserted
 		o_cs_roml  <= '1';
@@ -78,10 +109,16 @@ begin
 	end if;
 end process;
 
-s_addr_sw(0) <= i_lds;
-s_addr_sw(1) <= i_uds;
-s_addr_sw(2) <= addr(0);
-s_addr_sw(3) <= addr(1);
+-- enable output only when address is valid
+o_oe <= i_as;
+
+-- bind multiplexer inputs
+s_in_sw(3) <= i_addr(1);
+s_in_sw(2) <= i_addr(0);
+s_in_sw(1) <= i_uds;
+s_in_sw(0) <= i_lds;
+
+-- bind berr output to intermediate berr signal
+o_berr <= s_berr;
 
 end behavioral;
-
